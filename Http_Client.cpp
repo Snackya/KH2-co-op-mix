f5127c8770389bf1e6e284485a07a5e5aad3d568
Snackya <85370185+Snackya@users.noreply.github.com>
2021-05-28 03:11:44 +0200

    Update Http_Client.cpp

--- Http_Client.cpp
+++ Http_Client.cpp
@@ -7,7 +7,6 @@ void Http_Client::send_checks(std::map<uint16_t, uint8_t>& checks)
 	// so we need to recreate the client here
 	delete cli;
 	cli = new httplib::Client(server_addresss.c_str());
-	std::cout << "sending\n";
 	std::string package = Util::map_to_string(checks);
 	std::cout << "package:\n" << package << std::endl;
 	auto foo = cli->Post("/data", client_id, package, "text/plain");
@@ -15,20 +14,17 @@ void Http_Client::send_checks(std::map<uint16_t, uint8_t>& checks)
 
 std::map<uint16_t, uint8_t> Http_Client::request_checks()
 {
-	std::cout << "requesting\n";
 	std::map<uint16_t, uint8_t> checks;
 	auto res = cli->Get("/request", client_id);
-	if (!res) return checks;
+
+	if (!res)
+	{
+		return checks;
+	}
 
 	std::string response = res->body.c_str();
-
 	checks = Util::string_to_map(response);
-	std::cout << "response:\n" << response << std::endl;
-	std::cout << "endresult with size " << checks.size() << std::endl;
-	for (auto f : checks)
-	{
-		std::cout << f.first << " "; Util::print_byte(f.second);
-	}
+
 	return checks;
 }
 
@@ -41,6 +37,6 @@ void Http_Client::init(std::string server_addr)
 	auto res = cli->Get("/register");
 	if (!res) return;
 	std::string id = res->body.c_str();
-	std::cout << "received id " << id << "from server" << std::endl;
+	std::cout << "received id " << id << " from server" << std::endl;
 	client_id = { {"ID", id} };
 }
